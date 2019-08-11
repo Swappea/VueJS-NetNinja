440db8b5f48ed8575f502a455130029cb332d191
swappea <swapneshsangle1@gmail.com>
2019-08-11 20:51:59 +0530

    Section 10

---
+++ Projects/Project-1/app.js
@@ -0,0 +1,55 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        name: 'Swapnesh',
+        age: 23,
+        website: 'http://www.google.com',
+        websiteTag: '<a href="http://www.google.com">Google(v-html)</a>',
+        x: 0,
+        y: 0,
+        a: 0,
+        b: 0,
+        available: false,
+        nearby: false
+    },
+    methods: {
+        greet: function (time) {
+            return `Good ${time}`;
+        },
+        add: function (inc) {
+            this.age += inc;
+        },
+        subtract: function (dec) {
+            this.age -= dec;
+        },
+        updateXY: function (evt) {
+            this.x = evt.offsetX;
+            this.y = evt.offsetY;
+        },
+        click: function () {
+            alert('clicked');
+        },
+        logName: function() {
+            console.log('you entered your name');
+        },
+        logAge: function() {
+            console.log('you entered your age');
+        }
+    },
+    computed: {
+        addToA: function() {
+            console.log('addToA');
+            return this.a + this.age;
+        },
+        addToB: function() {
+            console.log('addToB');
+            return this.b + this.age;
+        },
+        compClasses: function() {
+            return {
+                available: this.available,
+                nearby: this.nearby,
+            };
+        }
+    },
+});
